139b7688339055e5b40031ce3194fc0414a0c97f
zhanglh <zhanglh@ename.com>
2017-12-25 15:52:32 +0800

    登陆demo

--- controllers/default.go
+++ controllers/default.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"MyBlog/models"
+	"fmt"
 )
 
 type MainController struct {
@@ -9,7 +11,18 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+	sess := c.StartSession()
+	username := sess.Get("username")
+	fmt.Println(username)
 	c.TplName = "index.tpl"
+}
+
+func (c *MainController)Post(){
+	var user models.User
+	intputs := c.Input()
+	user.Username = intputs.Get("username")
+	user.Pwd = intputs.Get("pwd")
+	fmt.Println(user)
 }
